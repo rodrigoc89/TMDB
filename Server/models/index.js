9c17cb992976d767fe2630839613643adbcd6646
rodrigoc89 <rodrigojoaquinc89@gmail.com>
2023-04-28 13:01:38 -0300

    server deleted

--- Server/models/index.js
+++
@@ -1,5 +0,0 @@
-const User = require("./User");
-const Favorites = require("./Favorites");
-
-User.hasMany(Favorites);
-Favorites.belongsTo(User);
--- Server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const routerUsers = require("./user");
-
-const router = Router();
-
-router.use("/users", routerUsers);
-
-module.exports = router;
